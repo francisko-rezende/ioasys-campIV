ad3fd0dfc0add29ead63aa71deb13c1a40738c34
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2021-12-04 18:39:59 -0300

    Add morph animation and final slide

--- script/script.js
+++ script/script.js
@@ -24,3 +24,12 @@ faders.forEach(fader => {
 sliders.forEach(slider => {
   appearOnScroll.observe(slider)
 })
+
+const tween = KUTE.fromTo(
+  '#blob1',
+  { path: '#blob1'},
+  { path: '#blob2'},
+  { repeat: 999, duration: 3000, yoyo: true}
+)
+
+tween.start()
