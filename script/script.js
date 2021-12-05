af314de5791c47cbb5cbca0fba515ae23cac6103
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2021-12-04 21:18:33 -0300

    Make ypewriter animation start on scroll

--- script/script.js
+++ script/script.js
@@ -1,11 +1,23 @@
 const faders = document.querySelectorAll('.fade-in')
 const sliders = document.querySelectorAll('[data-appear="slide-in"]')
+const typewriterText = document.querySelector('[data-js="typewriter"]')
 
 const appearOptions = {
   threshold: 0,
   rootMargin: "0px 0px -100px 0px"
 
 }
+
+const delayAnimation = new IntersectionObserver((text, delayAnimation) => {
+  if(!text[0].isIntersecting) {
+    return
+  }
+  text[0].target.classList.remove('hidden')
+  text[0].target.classList.add('typewriter-text')
+  delayAnimation.unobserve(text[0].target)
+}, appearOptions)
+
+delayAnimation.observe(typewriterText)
 
 const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
   entries.forEach(entry => {
